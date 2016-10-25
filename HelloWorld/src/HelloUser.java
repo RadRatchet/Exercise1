5e0762f909bdaeeae8bb62329c3277f62dae4651
Stefan Aicher <ga46yaj@mytum.de>
2016-10-25 20:44:01 +0200

    added HelloUser.java

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,12 @@
+
+public class HelloUser {
+	private String userName;
+	
+	public HelloUser(String name) {
+		userName = name;
+	}
+	
+	public void greetUser() {
+		System.out.println("Hello "+userName+"!");
+	}
+}
